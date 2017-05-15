e81eb3625516af9afa460ce53277483b5b0f8932
Mario de Biase <debiasej@gmail.com>
2017-05-15 19:23:37 +0200

    Create Song List component

--- client/index.js
+++ client/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import SongList from './components/song_list';
+
 // ApolloClient assumes that server had defined 'graphql' route
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <SongList />
     </ApolloProvider>
   );
 };
