671adbdb403a8ce98c8bd63ba16db22ab6b54bf8
Mario de Biase <debiasej@gmail.com>
2017-05-16 21:51:38 +0200

    Handling routes with react-router

--- client/index.js
+++ client/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
+import App from "./components/app";
 import SongList from './components/song_list';
+import SongCreate from './components/song_create';
 
 // ApolloClient assumes that server had defined 'graphql' route
 const client = new ApolloClient({});
@@ -11,7 +14,12 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="song/new" component={SongCreate} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
