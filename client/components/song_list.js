9035224839d50412d8013e39f719d6d16cbc84ca
Mario de Biase <debiasej@gmail.com>
2017-05-16 21:19:52 +0200

    Query Songs and render them

--- client/components/song_list.js
+++ client/components/song_list.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import gpl from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li>
+          {song.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    if (this.props.data.loading) { return <div>Loading...</div>; }
+
     return(
       <div>
-        Song List
+        {this.renderSongs()}
       </div>
     );
   }
 }
 
-export default SongList;
+const query = gpl`
+    {
+      songs {
+        title
+      }
+    }
+`;
+
+export default graphql(query)(SongList);
