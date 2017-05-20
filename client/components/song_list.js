ed28f08cd462fd190946467dbf79904c0c7e26e4
Mario de Biase <debiasej@gmail.com>
2017-05-20 20:59:33 +0200

    Delete a song

--- client/components/song_list.js
+++ client/components/song_list.js
@@ -6,11 +6,21 @@ import { Link } from 'react-router';
 import query from '../queries/fetch_songs';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({
+      variables: { id } })
+    .then(() => this.props.data.refetch());
+    // refetch all queries associated with the component
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className='collection-item'>
-          {song.title}
+        <li key={id} className='collection-item'>
+          {title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
         </li>
       );
     });
@@ -35,4 +45,14 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+const mutation = gpl`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
