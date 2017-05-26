8f8694b82a82935807ee63b8623349d610f0c4cb
Mario de Biase <debiasej@gmail.com>
2017-05-27 00:53:23 +0200

    Mutation that creates a new lyric

--- client/components/lyric_create.js
+++ client/components/lyric_create.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gpl from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricCreate extends Component {
   constructor(props){
@@ -9,7 +11,13 @@ class LyricCreate extends Component {
 
   onSubmit() {
     event.preventDefault();
-    
+
+    this.props.mutate({
+      variables: {
+        content: this.state.content,
+        songId: this.props.songId
+      }
+    }).then(() => this.setState({ content: '' }))
   }
 
   render() {
@@ -25,4 +33,15 @@ class LyricCreate extends Component {
   }
 }
 
-export default LyricCreate
+const mutation = gpl`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id,
+      lyrics {
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
